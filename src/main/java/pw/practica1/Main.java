799610d29025cc23f7d8591b0b088147d58279d2
Osvaldo Fernández <osvaldof22@hotmail.com>
2019-09-16 01:04:47 -0400

    Actualizando archivo Gradle

--- src/main/java/pw/practica1/Main.java
+++ src/main/java/pw/practica1/Main.java
@@ -1,4 +1,32 @@
 package pw.practica1;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        //Document type is an object that can contains a hold HTML Document
+        //Documentation here https://jsoup.org/apidocs/org/jsoup/nodes/Document.html
+        Document document = Jsoup.connect("http://localhost:4567/").get();
+        System.out.println("A) Lines: " + lineCount(document));
+        System.out.println("B) Parragraphs: " + parragraphCount(document));
+        System.out.println("C) Images: " + imageCount(document));
+    }
+
+    private static int lineCount(Document document){
+        return 0;
+    }
+
+    private static int parragraphCount(Document document){
+        Elements elements = document.getElementsByTag("p");
+        return elements.size();
+    }
+
+    private static int imageCount(Document document){
+        Elements elements = document.getElementsByTag("img");
+        return elements.size();
+    }
 }
